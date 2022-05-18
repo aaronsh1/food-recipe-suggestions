6c085ae47cc3fe710e95301d75e69a04bea00a03
Sean Groenewald <seang@bbd.co.za>
2022-05-18 21:51:30 +0200

    Updated datasource.js to use variables from dotenv

--- BackEnd/database/datasource.js
+++ BackEnd/database/datasource.js
@@ -20,9 +20,9 @@ const getModelDefinitions = () => {
 };
 
 const initialize = async () => {
-  sequelize = new Sequelize('food', 'root', 'root', {
-    host: 'localhost',
-    port: 3306,
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
   });
 
