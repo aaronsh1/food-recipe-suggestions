3ee44235234f8f09c3442b58069c3f2fbb91c193
schej002 <johannes.schepers@vcontractor.co.za>
2022-05-18 19:34:06 +0200

    Added env file setup.

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const database = require('./database/datasource')
+const dotenv = require('dotenv');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+
+dotenv.config();
 
 const app = express();
 
@@ -15,7 +17,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
