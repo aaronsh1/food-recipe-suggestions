23067806a3ca8d1e8f84a93904c070710440ed90
aaronsh <aaronsh@bbd.co.za>
2022-05-19 11:24:12 +0200

    Added configuration to import ingredient and modal components correctly.

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,4 @@
 import { AppRoot } from './lit-components';
-import { PantryIngredient } from "./lit-components/pantry/ingredient";
-import { AddIngredientModal } from './lit-components/pantry/modal';
 
 const root = new AppRoot();
 document.body.appendChild(root);
